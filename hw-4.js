e911f696cc02304a2670995a6b37eb8b82983a7a
igrushkina92 <a.igrushkina@mail.ru>
2025-05-31 14:21:59 +0300

    Выполнено задание по JS

---
+++ hw-4.js
@@ -0,0 +1,86 @@
+// task 1 
+
+let a = 10;
+alert (a);
+a = 20;
+alert (a);
+
+// task 2 
+
+const dateIphone = 2007;
+alert (dateIphone);
+
+// task 3 
+
+const nameJS = 'Brendan Eich';
+alert (nameJS);
+
+// task 4 
+
+let c = 10;
+let b = 2;
+alert (c+b);
+alert (c-b);
+alert (c*b);
+alert (c/b);
+
+// task 5 
+
+let result = 2**5;
+alert (result);
+
+// task 6 
+
+let d=9;
+let e=2;
+let rest = d%e;
+alert (rest);
+
+// task 7
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+// task 8
+
+let age = prompt ('Сколько вам лет?');
+alert (age);
+
+// task 9
+
+const user = {
+    name: 'Sasha',
+    age: 25,
+    isAdmin: true,
+}
+
+// task 10
+
+let  userName = prompt ('Твое имя')
+alert (`Привет, ${userName}!`)
+
+
+// Дополнительное задание
+
+// 1
+
+let number = prompt ('Введите любое число');
+alert (number);
+
+let rest1 = number*2;
+alert (rest1);
+
+let rest2 = rest1 + 10;
+alert (rest2);
+
+let rest3 = rest2 / 2;
+alert (rest3);
+
+let rest4 = rest3 - number;
+alert (rest4);
