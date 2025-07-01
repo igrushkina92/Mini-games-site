0d49a08fa74451a7a42effcd0b4982a31716b371
igrushkina92 <a.igrushkina@mail.ru>
2025-07-01 11:33:00 +0300

    Добавлены задания 1-2 в hw-9

--- hw-9.js
+++ hw-9.js
@@ -1,2 +1,16 @@
-// Задание 1
+// // Задание 1
 
+// let str = 'js';
+// str = str.toUpperCase();
+// console.log (str);
+
+
+// // Задание 2
+
+// function filter(arr, str) {
+//     const lowerStr = str.toLowerCase();
+//     return arr.filter (item => item.toLowerCase().startsWith(lowerStr));
+
+// }
+
+// console.log (filter(['JavaScript', 'Java', 'Pithon'], 'ja'))
