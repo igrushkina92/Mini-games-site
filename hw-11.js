244a2366b4cd52709178cf0886ae416311458f03
igrushkina92 <a.igrushkina@mail.ru>
2025-07-10 17:30:01 +0300

    Исправлены замечания наставника по анимациям в курсовой работе

--- hw-11.js
+++ hw-11.js
@@ -1,87 +1,97 @@
-// Задание 1   
+// // Задание 1   
 
-   const button1 = document.querySelector('.button1');
-    const title1 = document.querySelector('.title1');
+//    const button1 = document.querySelector('.button1');
+//     const title1 = document.querySelector('.title1');
 
-button1.addEventListener('click', () => {
-  if (title1.style.display === 'none') {
-   title1.style.display = 'block';
-  } else {
-    title1.style.display = 'none';
-  }
-});
-
-
-
-
-// Задание 2
-
-const button2 = document.querySelector('.button2');
-const description2 = document.querySelector('.description2');
-
-button2.addEventListener('click', () => {
-  description2.style.color = 'blue';
-});
+// button1.addEventListener('click', () => {
+//   if (title1.style.display === 'none') {
+//    title1.style.display = 'block';
+//   } else {
+//     title1.style.display = 'none';
+//   }
+// });
 
 
 
 
-// Задание 3
+// // Задание 2
 
-const button3 = document.querySelector('.button3');
-const title3 = document.querySelector('.title3');
+// const button2 = document.querySelector('.button2');
+// const description2 = document.querySelector('.description2');
 
-button3.addEventListener('click', () => {
-  title3.textContent = 'Привет, мир!';
-});
-
-
-
-
-// Задание 4
-
-const allDescriptions4 = document.querySelectorAll('.description4');
-
-allDescriptions4.forEach(description4 => {
-   description4.textContent = 'Измененный текст';
-});
+// button2.addEventListener('click', () => {
+//   description2.style.color = 'blue';
+// });
 
 
 
 
-// Задание 5
+// // Задание 3
 
-const allDescriptions5 = document.querySelectorAll('.description5');
+// const button3 = document.querySelector('.button3');
+// const title3 = document.querySelector('.title3');
 
-allDescriptions5.forEach(description5 => {
-   description5.textContent = 'НОВЫЙ ТЕКСТ';
-});
-
-
-
-
-// Задание 6
-
-const button6 = document.querySelector('.button6');
-const description6 = document.querySelector('.description6');
-
-button6.addEventListener('click', () => {
-    const newText6 = document.createElement('p');
-    newText6.textContent = 'Новый абзац';
-    newText6.classList.add ('text');
-    description6.appendChild (newText6);
-});
+// button3.addEventListener('click', () => {
+//   title3.textContent = 'Привет, мир!';
+// });
 
 
 
 
+// // Задание 4
 
-// Задание 7
+// const allDescriptions4 = document.querySelectorAll('.description4');
 
-const button7 = document.querySelector('.button7');
-const description7 = document.querySelector('.description7');
+// allDescriptions4.forEach(description4 => {
+//    description4.textContent = 'Измененный текст';
+// });
 
-button7.addEventListener('click', () => {
-   description7.remove();
 
-});
+
+
+// // Задание 5
+
+// const allDescriptions5 = document.querySelectorAll('.description5');
+
+// allDescriptions5.forEach(description5 => {
+//    description5.textContent = 'НОВЫЙ ТЕКСТ';
+// });
+
+
+
+
+// // Задание 6
+
+// const button6 = document.querySelector('.button6');
+// const description6 = document.querySelector('.description6');
+
+// button6.addEventListener('click', () => {
+//     const newText6 = document.createElement('p');
+//     newText6.textContent = 'Новый абзац';
+//     newText6.classList.add ('text');
+//     description6.appendChild (newText6);
+// });
+
+
+
+
+
+// // Задание 7
+
+// const button7 = document.querySelector('.button7');
+// const description7 = document.querySelector('.description7');
+
+// button7.addEventListener('click', () => {
+//    description7.remove();
+
+// });
+
+
+const user = {
+   name: 'Bob',
+   age: 32
+};
+
+let info = prompt("Что вы хотите узнать о пользователе?", "name");
+
+console.log(user[info]);
